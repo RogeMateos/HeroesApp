2cf71beba58aa768440d805785c2b874c4fe82cc
Roge <rogemateos@hotmail.com>
2025-03-05 12:32:57 +0000

    Adding locig for alter ego

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export const HeroCard = ({
   id,
   superhero,
@@ -11,6 +11,14 @@ export const HeroCard = ({
   
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
   
+// Internal component for conditional character display
+const CharactersByHero = ({ alter_ego, characters }) => {
+  // If alter_ego and characters are the same, don't render anything
+  if (alter_ego === characters) return <></>;
+  
+  // Otherwise, return the characters paragraph
+  return <p>{ characters }</p>;
+};
   return (
     <div className="col">
       <div className="card">
@@ -25,7 +33,16 @@ export const HeroCard = ({
               <h5 className="card-title">{ superhero }</h5>
               <p className="card-text">{ alter_ego }</p>
               
-              <p>{ characters }</p>
+              {/* Using the internal component instead of inline conditional */}
+              <CharactersByHero alter_ego={alter_ego} characters={characters} />
+              
+              <p className="card-text">
+                <small className="text-muted">{ first_appearance }</small>
+              </p>
+              
+              <Link to={`/hero/${id}`}>
+                Más información
+              </Link>
             </div>
           </div>
         </div>
